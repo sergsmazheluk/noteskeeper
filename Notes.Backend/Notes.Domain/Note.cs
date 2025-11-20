86a73b63b13cc899cc2d0495c092baf2a83c40cd
Serzh <smazheluks@gmail.com>
2025-11-20 23:30:37 +0300

    feat(domain): add Note entity

---
+++ Notes.Backend/Notes.Domain/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid Userid { get; set; }
+        public Guid Noteid { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+}
